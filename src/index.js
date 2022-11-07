f7dac14cd73fa474a760534d466efa9cd47359bd
Pratt <pratt.paredes@gmail.com>
2022-11-07 13:48:23 -0500

    adding enviroment variable

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 9000;
@@ -7,5 +9,8 @@ const port = process.env.PORT || 9000;
 app.get("/", (req, res) => {
   res.send("Welcome to my API");
 });
+
+// mongodb connection
+mongoose.connect();
 
 app.listen(port, () => console.log("server listening on port ", port));
